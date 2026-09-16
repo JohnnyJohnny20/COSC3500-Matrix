287a9bb934959307ece91473fca2288b9150e5f6
s4844823 <s4844823@login0.compute.eait.uq.edu.au>
2026-09-16 19:26:57 +1000

    Naive implementation of matrix multiply. No optimizations

--- matrixMultiply.cpp
+++ matrixMultiply.cpp
@@ -12,10 +12,20 @@
 * @return : your student ID
 *				 			 	    	 		   			 	      
 * */
-int matrixMultiply(int N, const floatType* A, const floatType* B, floatType* C, int* args, int argCount){				 			 	    	 		   			 	      
+int matrixMultiply(int N, const floatType* A, const floatType* B, floatType* C, int* args, int argCount) {		
 if (N<=0) { return STUDENTID;}//Your code must be able to deal with N=0 scenario without crashing.				 			 	    	 		   			 	      
-
 //WRITE YOUR CODE HERE
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			floatType sum = 0;
+			for (int  k = 0; k < N; k++) {
+				sum += A[i * N + k] * B[k * N + j];
+			}
+			C[i * N + j] = sum;
+		}
+	}
+
 
 return STUDENTID;				 			 	    	 		   			 	      
 
