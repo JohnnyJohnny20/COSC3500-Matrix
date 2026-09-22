1ba338521522c3d7bdd445e94d55b6b66f3b99a3
s4844823 <s4844823@login0.compute.eait.uq.edu.au>
2026-09-23 08:03:20 +1000

    perf: treat B as pre-transposed via indexing, restructure for row-major C

--- matrixMultiply.cpp
+++ matrixMultiply.cpp
@@ -39,17 +39,20 @@ int matrixMultiply(int N, const floatType* A, const floatType* B, floatType* C, 
 if (N<=0) { return STUDENTID;}//Your code must be able to deal with N=0 scenario without crashing.				 			 	    	 		   			 	      
 //WRITE YOUR CODE HERE
 	
-	int jStart = 0;
-	int jEndLimit = N;
+	const floatType* X = B;
+	const floatType* Y = A;
+	
+	int iStart = 0;
+	int iEndLimit = N;
 	
 	if (argCount == 2) {
-        	jStart = args[0];
-        	jEndLimit = args[1];
+        	iStart = args[0];
+        	iEndLimit = args[1];
     	}
 		
 	#pragma omp parallel for num_threads(4)
-	for (int j = jStart; j < jEndLimit ; j++) {
-		memset(C + j * N, 0, N * sizeof(floatType));
+	for (int i = iStart; i < iEndLimit ; i++) {
+		memset(C + i * N, 0, N * sizeof(floatType));
 	}
 
 	const int blockJ = 32, blockK = 32;
@@ -57,62 +60,64 @@ if (N<=0) { return STUDENTID;}//Your code must be able to deal with N=0 scenario
 	
 	#pragma omp parallel for num_threads(4)
 	
-	for (int jj = jStart; jj < jEndLimit; jj += blockJ) {
-        int jEnd = jj + blockJ < jEndLimit ? jj + blockJ : jEndLimit;
-        for (int kk = 0; kk < N; kk += blockK) {
-                int kEnd = kk + blockK < N ? kk + blockK : N;
-                for (int j = jj; j < jEnd; j += 4) {          // widened to 4 at a time
-                        for (int k = kk; k < kEnd; k+=4) {      
-                                __m256 bVec0 = broadcastComplex(B[(j+0)*N+k]);
-                                __m256 bVec1 = broadcastComplex(B[(j+1)*N+k]);
-                                __m256 bVec2 = broadcastComplex(B[(j+2)*N+k]);
-                                __m256 bVec3 = broadcastComplex(B[(j+3)*N+k]);
-				__m256 bSw0 = _mm256_permute_ps(bVec0, 0xB1);
-				__m256 bSw1 = _mm256_permute_ps(bVec1, 0xB1);
-				__m256 bSw2 = _mm256_permute_ps(bVec2, 0xB1);
-				__m256 bSw3 = _mm256_permute_ps(bVec3, 0xB1);
+	for (int ii = iStart; ii < iEndLimit; ii += blockI) {
+		int iEnd = ii + blockI < iEndLimit ? ii + blockI : iEndLimit;
 
-				__m256 bVec4 = broadcastComplex(B[(j+0)*N+k+1]);
-                                __m256 bVec5 = broadcastComplex(B[(j+1)*N+k+1]);
-                                __m256 bVec6 = broadcastComplex(B[(j+2)*N+k+1]);
-                                __m256 bVec7 = broadcastComplex(B[(j+3)*N+k+1]);
-                                __m256 bSw4 = _mm256_permute_ps(bVec4, 0xB1);
-                                __m256 bSw5 = _mm256_permute_ps(bVec5, 0xB1);
-                                __m256 bSw6 = _mm256_permute_ps(bVec6, 0xB1);
-                                __m256 bSw7 = _mm256_permute_ps(bVec7, 0xB1);
-				
-				// --- Precompute B for step k+2 ---
-                                __m256 bVec8 = broadcastComplex(B[(j+0)*N+k+2]);
-                                __m256 bVec9 = broadcastComplex(B[(j+1)*N+k+2]);
-                                __m256 bVec10 = broadcastComplex(B[(j+2)*N+k+2]);
-                                __m256 bVec11 = broadcastComplex(B[(j+3)*N+k+2]);
-                                __m256 bSw8 = _mm256_permute_ps(bVec8, 0xB1);
-                                __m256 bSw9 = _mm256_permute_ps(bVec9, 0xB1);
-                                __m256 bSw10 = _mm256_permute_ps(bVec10, 0xB1);
-                                __m256 bSw11 = _mm256_permute_ps(bVec11, 0xB1);
+        	for (int kk = 0; kk < N; kk += blockK) {
+                	int kEnd = kk + blockK < N ? kk + blockK : N;
+	
+				for (int i = ii; i + 4 <= iEnd; i += 4) {          // widened to 4 at a time
+	                        	for (int k = kk; k < kEnd; k+=4) {      
+	                                __m256 bVec0 = broadcastComplex(X[(i+0)*N+k]);
+	                                __m256 bVec1 = broadcastComplex(X[(i+1)*N+k]);
+	                                __m256 bVec2 = broadcastComplex(X[(i+2)*N+k]);
+	                                __m256 bVec3 = broadcastComplex(X[(i+3)*N+k]);
+					__m256 bSw0 = _mm256_permute_ps(bVec0, 0xB1);
+					__m256 bSw1 = _mm256_permute_ps(bVec1, 0xB1);
+					__m256 bSw2 = _mm256_permute_ps(bVec2, 0xB1);
+					__m256 bSw3 = _mm256_permute_ps(bVec3, 0xB1);
 
-                                // --- Precompute B for step k+3 ---
-                                __m256 bVec12 = broadcastComplex(B[(j+0)*N+k+3]);
-                                __m256 bVec13 = broadcastComplex(B[(j+1)*N+k+3]);
-                                __m256 bVec14 = broadcastComplex(B[(j+2)*N+k+3]);
-                                __m256 bVec15 = broadcastComplex(B[(j+3)*N+k+3]);
-                                __m256 bSw12 = _mm256_permute_ps(bVec12, 0xB1);
-                                __m256 bSw13 = _mm256_permute_ps(bVec13, 0xB1);
-                                __m256 bSw14 = _mm256_permute_ps(bVec14, 0xB1);
-                                __m256 bSw15 = _mm256_permute_ps(bVec15, 0xB1);
-				
-                                for (int ii = 0; ii < N; ii += blockI) {
-                                        int iEnd = ii + blockI < N ? ii + blockI : N;
-                                        for (int i = ii; i + 4 <= iEnd; i += 4) {
+					__m256 bVec4 = broadcastComplex(X[(i+0)*N+k+1]);
+	                                __m256 bVec5 = broadcastComplex(X[(i+1)*N+k+1]);
+	                                __m256 bVec6 = broadcastComplex(X[(i+2)*N+k+1]);
+	                                __m256 bVec7 = broadcastComplex(X[(i+3)*N+k+1]);
+	                                __m256 bSw4 = _mm256_permute_ps(bVec4, 0xB1);
+	                                __m256 bSw5 = _mm256_permute_ps(bVec5, 0xB1);
+	                                __m256 bSw6 = _mm256_permute_ps(bVec6, 0xB1);
+	                                __m256 bSw7 = _mm256_permute_ps(bVec7, 0xB1);
 						
-						__m256 c0 = _mm256_loadu_ps(reinterpret_cast<const float*>(C + (j+0)*N + i));
-                                                __m256 c1 = _mm256_loadu_ps(reinterpret_cast<const float*>(C + (j+1)*N + i));
-                                                __m256 c2 = _mm256_loadu_ps(reinterpret_cast<const float*>(C + (j+2)*N + i));
-                                                __m256 c3 = _mm256_loadu_ps(reinterpret_cast<const float*>(C + (j+3)*N + i));
+					// --- Precompute B for step k+2 ---
+	                                __m256 bVec8 = broadcastComplex(X[(i+0)*N+k+2]);
+	                                __m256 bVec9 = broadcastComplex(X[(i+1)*N+k+2]);
+	                                __m256 bVec10 = broadcastComplex(X[(i+2)*N+k+2]);
+	                                __m256 bVec11 = broadcastComplex(X[(i+3)*N+k+2]);
+	                                __m256 bSw8 = _mm256_permute_ps(bVec8, 0xB1);
+	                                __m256 bSw9 = _mm256_permute_ps(bVec9, 0xB1);
+	                                __m256 bSw10 = _mm256_permute_ps(bVec10, 0xB1);
+	                                __m256 bSw11 = _mm256_permute_ps(bVec11, 0xB1);
+
+	                                // --- Precompute B for step k+3 ---
+	                                __m256 bVec12 = broadcastComplex(X[(i+0)*N+k+3]);
+	                                __m256 bVec13 = broadcastComplex(X[(i+1)*N+k+3]);
+	                                __m256 bVec14 = broadcastComplex(X[(i+2)*N+k+3]);
+	                                __m256 bVec15 = broadcastComplex(X[(i+3)*N+k+3]);
+	                                __m256 bSw12 = _mm256_permute_ps(bVec12, 0xB1);
+	                                __m256 bSw13 = _mm256_permute_ps(bVec13, 0xB1);
+	                                __m256 bSw14 = _mm256_permute_ps(bVec14, 0xB1);
+	                                __m256 bSw15 = _mm256_permute_ps(bVec15, 0xB1);
+					
+                                	 for (int jj = 0; jj < N; jj += blockJ) {
+                                	int jEnd = jj + blockJ < N ? jj + blockJ : N;
+					for (int j = jj; j + 4 <= jEnd; j += 4) {
+	                                        
+						__m256 c0 = _mm256_loadu_ps(reinterpret_cast<const float*>(C + (i+0)*N + j));
+                                                __m256 c1 = _mm256_loadu_ps(reinterpret_cast<const float*>(C + (i+1)*N + j));
+                                                __m256 c2 = _mm256_loadu_ps(reinterpret_cast<const float*>(C + (i+2)*N + j));
+                                                __m256 c3 = _mm256_loadu_ps(reinterpret_cast<const float*>(C + (i+3)*N + j));
 						
 						__m256 aVec, aRe, aIm;
 
-                                                aVec = _mm256_loadu_ps(reinterpret_cast<const float*>(A + k*N + i)); // contiguous, loaded once, used 4x
+                                                aVec = _mm256_loadu_ps(reinterpret_cast<const float*>(Y + k*N + j)); // contiguous, loaded once, used 4x
 						aRe = _mm256_moveldup_ps(aVec);
         					aIm = _mm256_movehdup_ps(aVec);
 
@@ -121,7 +126,7 @@ if (N<=0) { return STUDENTID;}//Your code must be able to deal with N=0 scenario
                                                 c2 = fmaAddComplex(c2, aRe, aIm, bVec2, bSw2);
                                                 c3 = fmaAddComplex(c3, aRe, aIm, bVec3, bSw3);
 
-						aVec = _mm256_loadu_ps(reinterpret_cast<const float*>(A + (k+1)*N + i)); // contiguous, loaded once, used 4x
+						aVec = _mm256_loadu_ps(reinterpret_cast<const float*>(Y + (k+1)*N + j)); // contiguous, loaded once, used 4x
                                                 aRe = _mm256_moveldup_ps(aVec);
                                                 aIm = _mm256_movehdup_ps(aVec);
                                         	
@@ -130,7 +135,7 @@ if (N<=0) { return STUDENTID;}//Your code must be able to deal with N=0 scenario
                                                 c2 = fmaAddComplex(c2, aRe, aIm, bVec6, bSw6);
                                                 c3 = fmaAddComplex(c3, aRe, aIm, bVec7, bSw7);
 
-						aVec = _mm256_loadu_ps(reinterpret_cast<const float*>(A + (k+2)*N + i)); // contiguous, loaded once, used 4x
+						aVec = _mm256_loadu_ps(reinterpret_cast<const float*>(Y + (k+2)*N + j)); // contiguous, loaded once, used 4x
                                                 aRe = _mm256_moveldup_ps(aVec);
                                                 aIm = _mm256_movehdup_ps(aVec);
 
@@ -139,7 +144,7 @@ if (N<=0) { return STUDENTID;}//Your code must be able to deal with N=0 scenario
                                                 c2 = fmaAddComplex(c2, aRe, aIm, bVec10, bSw10);
                                                 c3 = fmaAddComplex(c3, aRe, aIm, bVec11, bSw11);
 
-						aVec = _mm256_loadu_ps(reinterpret_cast<const float*>(A + (k+3)*N + i)); // contiguous, loaded once, used 4x
+						aVec = _mm256_loadu_ps(reinterpret_cast<const float*>(Y + (k+3)*N + j)); // contiguous, loaded once, used 4x
                                                 aRe = _mm256_moveldup_ps(aVec);
                                                 aIm = _mm256_movehdup_ps(aVec);
 
@@ -148,10 +153,10 @@ if (N<=0) { return STUDENTID;}//Your code must be able to deal with N=0 scenario
                                                 c2 = fmaAddComplex(c2, aRe, aIm, bVec14, bSw14);
                                                 c3 = fmaAddComplex(c3, aRe, aIm, bVec15, bSw15);
 
-						_mm256_storeu_ps(reinterpret_cast<float*>(C + (j+0)*N + i), c0);
-                                                _mm256_storeu_ps(reinterpret_cast<float*>(C + (j+1)*N + i), c1);
-                                                _mm256_storeu_ps(reinterpret_cast<float*>(C + (j+2)*N + i), c2);
-                                                _mm256_storeu_ps(reinterpret_cast<float*>(C + (j+3)*N + i), c3);
+						_mm256_storeu_ps(reinterpret_cast<float*>(C + (i+0)*N + j), c0);
+                                                _mm256_storeu_ps(reinterpret_cast<float*>(C + (i+1)*N + j), c1);
+                                                _mm256_storeu_ps(reinterpret_cast<float*>(C + (i+2)*N + j), c2);
+                                                _mm256_storeu_ps(reinterpret_cast<float*>(C + (i+3)*N + j), c3);
 					}
                                 }
                         }
