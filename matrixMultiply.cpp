6bf9e0100bbb78a22ed3558d948749b1eeefd9eb
s4844823 <s4844823@login0.compute.eait.uq.edu.au>
2026-09-23 08:37:10 +1000

    refactor: extract 4x4 tile update into microKernel4x4 helper

--- matrixMultiply.cpp
+++ matrixMultiply.cpp
@@ -23,10 +23,62 @@ static inline __m256 fmaAddComplex(__m256 c, __m256 aRe, __m256 aIm, __m256 b, _
         return _mm256_addsub_ps(c_new, _mm256_mul_ps(aIm, bSw)); // c_new +/- aIm*bSw
 }
 
+// Refactored helper functions by Claude
 static inline void loadBRow(const floatType* B, int N, int row, int col, __m256& bVec, __m256& bSw) {
 	bVec = broadcastComplex(B[row * N + col]);
     	bSw  = _mm256_permute_ps(bVec, 0xB1);
 }
+
+static inline void microKernel4x4(const floatType* Y, int N, int j, int k, int i, floatType* C, const __m256 bVec[16], const __m256 bSw[16]) {
+	__m256 c0 = _mm256_loadu_ps(reinterpret_cast<const float*>(C + (i+0)*N + j));
+        __m256 c1 = _mm256_loadu_ps(reinterpret_cast<const float*>(C + (i+1)*N + j));
+        __m256 c2 = _mm256_loadu_ps(reinterpret_cast<const float*>(C + (i+2)*N + j));
+        __m256 c3 = _mm256_loadu_ps(reinterpret_cast<const float*>(C + (i+3)*N + j));
+
+        __m256 aVec, aRe, aIm;
+
+        aVec = _mm256_loadu_ps(reinterpret_cast<const float*>(Y + k*N + j)); // contiguous, loaded once, used 4x
+        aRe = _mm256_moveldup_ps(aVec);
+        aIm = _mm256_movehdup_ps(aVec);
+
+        c0 = fmaAddComplex(c0, aRe, aIm, bVec[0], bSw[0]);
+        c1 = fmaAddComplex(c1, aRe, aIm, bVec[1], bSw[1]);
+        c2 = fmaAddComplex(c2, aRe, aIm, bVec[2], bSw[2]);
+        c3 = fmaAddComplex(c3, aRe, aIm, bVec[3], bSw[3]);
+
+        aVec = _mm256_loadu_ps(reinterpret_cast<const float*>(Y + (k+1)*N + j)); // contiguous, loaded once, used 4x
+        aRe = _mm256_moveldup_ps(aVec);
+        aIm = _mm256_movehdup_ps(aVec);
+
+        c0 = fmaAddComplex(c0, aRe, aIm, bVec[4], bSw[4]);
+        c1 = fmaAddComplex(c1, aRe, aIm, bVec[5], bSw[5]);
+        c2 = fmaAddComplex(c2, aRe, aIm, bVec[6], bSw[6]);
+        c3 = fmaAddComplex(c3, aRe, aIm, bVec[7], bSw[7]);
+
+        aVec = _mm256_loadu_ps(reinterpret_cast<const float*>(Y + (k+2)*N + j)); // contiguous, loaded once, used 4x
+        aRe = _mm256_moveldup_ps(aVec);
+        aIm = _mm256_movehdup_ps(aVec);
+
+        c0 = fmaAddComplex(c0, aRe, aIm, bVec[8], bSw[8]);
+        c1 = fmaAddComplex(c1, aRe, aIm, bVec[9], bSw[9]);
+        c2 = fmaAddComplex(c2, aRe, aIm, bVec[10], bSw[10]);
+        c3 = fmaAddComplex(c3, aRe, aIm, bVec[11], bSw[11]);
+
+        aVec = _mm256_loadu_ps(reinterpret_cast<const float*>(Y + (k+3)*N + j)); // contiguous, loaded once, used 4x
+        aRe = _mm256_moveldup_ps(aVec);
+        aIm = _mm256_movehdup_ps(aVec);
+
+        c0 = fmaAddComplex(c0, aRe, aIm, bVec[12], bSw[12]);
+        c1 = fmaAddComplex(c1, aRe, aIm, bVec[13], bSw[13]);
+        c2 = fmaAddComplex(c2, aRe, aIm, bVec[14], bSw[14]);
+        c3 = fmaAddComplex(c3, aRe, aIm, bVec[15], bSw[15]);
+
+        _mm256_storeu_ps(reinterpret_cast<float*>(C + (i+0)*N + j), c0);
+        _mm256_storeu_ps(reinterpret_cast<float*>(C + (i+1)*N + j), c1);
+        _mm256_storeu_ps(reinterpret_cast<float*>(C + (i+2)*N + j), c2);
+        _mm256_storeu_ps(reinterpret_cast<float*>(C + (i+3)*N + j), c3);
+}
+
 
 /**
 * @brief Implements an NxN matrix multiply C=A*B
@@ -74,81 +126,31 @@ if (N<=0) { return STUDENTID;}//Your code must be able to deal with N=0 scenario
 				for (int i = ii; i + 4 <= iEnd; i += 4) {          // widened to 4 at a time
 	                        	for (int k = kk; k < kEnd; k+=4) {
 
-					__m256 bVec0, bVec1,bVec2, bVec3, bSw0, bSw1, bSw2, bSw3;
-	                                loadBRow(X, N, i+0, k, bVec0, bSw0);
-					loadBRow(X, N, i+1, k, bVec1, bSw1);
-					loadBRow(X, N, i+2, k, bVec2, bSw2);
-					loadBRow(X, N, i+3, k, bVec3, bSw3);
+					__m256 bVec[16], bSw[16];
+	                                loadBRow(X, N, i+0, k, bVec[0], bSw[0]);
+					loadBRow(X, N, i+1, k, bVec[1], bSw[1]);
+					loadBRow(X, N, i+2, k, bVec[2], bSw[2]);
+					loadBRow(X, N, i+3, k, bVec[3], bSw[3]);
 
-					__m256 bVec4, bVec5, bVec6, bVec7, bSw4, bSw5, bSw6, bSw7;
-					loadBRow(X, N, i+0, k+1, bVec4, bSw4);
-                                        loadBRow(X, N, i+1, k+1, bVec5, bSw5);
-                                        loadBRow(X, N, i+2, k+1, bVec6, bSw6);
-                                        loadBRow(X, N, i+3, k+1, bVec7, bSw7);
-					
-					__m256 bVec8, bVec9, bVec10, bVec11, bSw8, bSw9, bSw10, bSw11;
-                                        loadBRow(X, N, i+0, k+2, bVec8, bSw8);
-                                        loadBRow(X, N, i+1, k+2, bVec9, bSw9);
-                                        loadBRow(X, N, i+2, k+2, bVec10, bSw10);
-                                        loadBRow(X, N, i+3, k+2, bVec11, bSw11);
+					loadBRow(X, N, i+0, k+1, bVec[4], bSw[4]);
+                                        loadBRow(X, N, i+1, k+1, bVec[5], bSw[5]);
+                                        loadBRow(X, N, i+2, k+1, bVec[6], bSw[6]);
+                                        loadBRow(X, N, i+3, k+1, bVec[7], bSw[7]);
 
-					__m256 bVec12, bVec13, bVec14, bVec15, bSw12, bSw13, bSw14, bSw15;
-                                        loadBRow(X, N, i+0, k+3, bVec12, bSw12);
-                                        loadBRow(X, N, i+1, k+3, bVec13, bSw13);
-                                        loadBRow(X, N, i+2, k+3, bVec14, bSw14);
-                                        loadBRow(X, N, i+3, k+3, bVec15, bSw15);
+                                        loadBRow(X, N, i+0, k+2, bVec[8], bSw[8]);
+                                        loadBRow(X, N, i+1, k+2, bVec[9], bSw[9]);
+                                        loadBRow(X, N, i+2, k+2, bVec[10], bSw[10]);
+                                        loadBRow(X, N, i+3, k+2, bVec[11], bSw[11]);
+
+                                        loadBRow(X, N, i+0, k+3, bVec[12], bSw[12]);
+                                        loadBRow(X, N, i+1, k+3, bVec[13], bSw[13]);
+                                        loadBRow(X, N, i+2, k+3, bVec[14], bSw[14]);
+                                        loadBRow(X, N, i+3, k+3, bVec[15], bSw[15]);
 
                                 	for (int jj = 0; jj < N; jj += blockJ) {
                                 	int jEnd = jj + blockJ < N ? jj + blockJ : N;
 					for (int j = jj; j + 4 <= jEnd; j += 4) {
-	                                        
-						__m256 c0 = _mm256_loadu_ps(reinterpret_cast<const float*>(C + (i+0)*N + j));
-                                                __m256 c1 = _mm256_loadu_ps(reinterpret_cast<const float*>(C + (i+1)*N + j));
-                                                __m256 c2 = _mm256_loadu_ps(reinterpret_cast<const float*>(C + (i+2)*N + j));
-                                                __m256 c3 = _mm256_loadu_ps(reinterpret_cast<const float*>(C + (i+3)*N + j));
-						
-						__m256 aVec, aRe, aIm;
-
-                                                aVec = _mm256_loadu_ps(reinterpret_cast<const float*>(Y + k*N + j)); // contiguous, loaded once, used 4x
-						aRe = _mm256_moveldup_ps(aVec);
-        					aIm = _mm256_movehdup_ps(aVec);
-
-                                                c0 = fmaAddComplex(c0, aRe, aIm, bVec0, bSw0);
-                                                c1 = fmaAddComplex(c1, aRe, aIm, bVec1, bSw1);
-                                                c2 = fmaAddComplex(c2, aRe, aIm, bVec2, bSw2);
-                                                c3 = fmaAddComplex(c3, aRe, aIm, bVec3, bSw3);
-
-						aVec = _mm256_loadu_ps(reinterpret_cast<const float*>(Y + (k+1)*N + j)); // contiguous, loaded once, used 4x
-                                                aRe = _mm256_moveldup_ps(aVec);
-                                                aIm = _mm256_movehdup_ps(aVec);
-                                        	
-						c0 = fmaAddComplex(c0, aRe, aIm, bVec4, bSw4);
-                                                c1 = fmaAddComplex(c1, aRe, aIm, bVec5, bSw5);
-                                                c2 = fmaAddComplex(c2, aRe, aIm, bVec6, bSw6);
-                                                c3 = fmaAddComplex(c3, aRe, aIm, bVec7, bSw7);
-
-						aVec = _mm256_loadu_ps(reinterpret_cast<const float*>(Y + (k+2)*N + j)); // contiguous, loaded once, used 4x
-                                                aRe = _mm256_moveldup_ps(aVec);
-                                                aIm = _mm256_movehdup_ps(aVec);
-
-                                                c0 = fmaAddComplex(c0, aRe, aIm, bVec8, bSw8);
-                                                c1 = fmaAddComplex(c1, aRe, aIm, bVec9, bSw9);
-                                                c2 = fmaAddComplex(c2, aRe, aIm, bVec10, bSw10);
-                                                c3 = fmaAddComplex(c3, aRe, aIm, bVec11, bSw11);
-
-						aVec = _mm256_loadu_ps(reinterpret_cast<const float*>(Y + (k+3)*N + j)); // contiguous, loaded once, used 4x
-                                                aRe = _mm256_moveldup_ps(aVec);
-                                                aIm = _mm256_movehdup_ps(aVec);
-
-                                                c0 = fmaAddComplex(c0, aRe, aIm, bVec12, bSw12);
-                                                c1 = fmaAddComplex(c1, aRe, aIm, bVec13, bSw13);
-                                                c2 = fmaAddComplex(c2, aRe, aIm, bVec14, bSw14);
-                                                c3 = fmaAddComplex(c3, aRe, aIm, bVec15, bSw15);
-
-						_mm256_storeu_ps(reinterpret_cast<float*>(C + (i+0)*N + j), c0);
-                                                _mm256_storeu_ps(reinterpret_cast<float*>(C + (i+1)*N + j), c1);
-                                                _mm256_storeu_ps(reinterpret_cast<float*>(C + (i+2)*N + j), c2);
-                                                _mm256_storeu_ps(reinterpret_cast<float*>(C + (i+3)*N + j), c3);
+	                                        microKernel4x4(Y, N, j, k, i, C, bVec, bSw);
 					}
                                 }
                         }
